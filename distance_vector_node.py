ec5899e43f8e1794bb2fc5c62279f81ed639fc1f
zhuk48 <33162506+zhuk48@users.noreply.github.com>
2022-02-16 18:48:52 -0600

    started DV

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -1,9 +1,14 @@
 from simulator.node import Node
 
+#Why doesn't Python have structs like C???
+#DV list:
+# [cost, next_hop, timestamp]
 
 class Distance_Vector_Node(Node):
     def __init__(self, id):
         super().__init__(id)
+        self.dist = {} # dictionary to hold distances and paths to given node
+        self.links = {} # dictionary containing all linkes connected to given node
 
     # Return a string
     def __str__(self):
@@ -12,6 +17,7 @@ class Distance_Vector_Node(Node):
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
         # latency = -1 if delete a link
+        self.recalculate_dist(neighbor, latency)
         pass
 
     # Fill in this function
@@ -20,4 +26,12 @@ class Distance_Vector_Node(Node):
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        return -1
+        temp_set = frozenset(self.id, destination)
+        if temp_set in self.dist.keys():
+            return self.dist[temp_set]
+        else:
+            return -1
+
+    # recalculates distances to all nodes after reciving new information
+    def recalculate_dist(self, updated_link, updated_lat):
+        pass
