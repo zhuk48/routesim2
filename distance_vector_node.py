48759eff4ff40f969002cd4163676423b6bef9c8
zhuk48 <33162506+zhuk48@users.noreply.github.com>
2022-02-18 11:20:33 -0600

    PASSING CASE 2

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -20,91 +20,45 @@ class Distance_Vector_Node(Node):
         super().__init__(id)
         self.dist = {} # dictionary to hold distances and paths to given node
         self.dist[self.id] = dv(0, 0, [self.id])
+        self.ndist = {} # dictionary to hold DVs of neighbors
 
     # Return a string
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        o = "Node: " + str(self.id) + "DV: " + str(self.dv) 
+        return o
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        #print(self.dist)
-        #for key in self.dist:
-            #print(self.dist[key].path)
         if neighbor in self.dist: #link already exists, updating value
             # latency = -1 if delete a link
             if latency == -1:
                 del self.dist[neighbor]
+                del self.ndist[neighbor]
             else:
-                if self.dist[neighbor].cost > latency:
-                    #self.dist[neighbor].cost = latency
-                    #self.dist[neighbor].path = [self.id, neighbor]
-                    #self.dist[neighbor].seq += 1
-                    # RECALCULATE DV! HOW???
-                    self.broadcast_change(0)
-                elif latency > self.dist[neighbor].cost:
-                    self.dist[neighbor].cost = latency
-                    self.dist[neighbor].path = []
-                    self.dist[neighbor].seq += 1
-                    self.broadcast_change(0)
+                self.dist[neighbor].cost = latency
+                self.dist[neighbor].seq += 1
                      
         else: #link DNE, create new one
             self.dist[neighbor] = dv(latency, 0, [self.id, neighbor])
-            self.broadcast_change(0)
+        
+        self.recalculate_dist()
+        self.broadcast_change(0)
 
     def process_incoming_routing_message(self, m):
-        num, new_table = json.loads(m)
-        n = int(num)
+        n, new_table = json.loads(m)
         new_table = json.loads(new_table)
         # converting json back to class
-        print("INCOMING TABLE from node" + str(n))
         for key in new_table:
             new_table[key] = dv(new_table[key]['cost'], new_table[key]['seq'], new_table[key]['path'])
-            print(key)
-            print(new_table[key].cost)
-            print(new_table[key].path)
-            print(new_table[key].seq)
-        #print(new_table)
 
         # JSONs suck and this line converts keys in new_table from strings to ints
+        # this line was taken from stack overflow
         new_table = {int(key):value for key, value in new_table.items()}
+        
+        # updating incoming neighbor DV in self.ndist
+        self.ndist[n] = new_table
 
-        print("CURRENT TABLE for node " + str(self.id))
-        for key in self.dist:
-            print(key)
-            print(self.dist[key].cost)
-            print(self.dist[key].path)
-            print(self.dist[key].seq)
-
-        dv_updated = False   
-        for key in new_table:
-            if key not in self.dist:
-                dv_updated = True
-                # link in incoming message not in current table
-                # add to current table
-                self.dist[key] = copy.deepcopy(new_table[key])
-                self.dist[key].path.insert(0, self.id)
-                self.dist[key].cost += self.dist[n].cost 
-                #print(new_table[key].path)
-            else:
-                if (new_table[key].seq > self.dist[key].seq) and key != n:
-                    dv_updated = True
-                    self.dist[key] = copy.deepcopy(new_table[key])
-                    self.dist[key].cost += self.dist[n].cost
-                    self.dist[key].path.insert(0,self.id)
-                # link in current table, need to update
-                if self.dist[key].cost > self.dist[n].cost + new_table[key].cost:
-                    dv_updated = True
-                    self.dist[key] = copy.deepcopy(new_table[key])
-                    self.dist[key].cost += self.dist[n].cost
-                    self.dist[key].path.insert(0,self.id)
-
-        print("NEW TABLE for node " + str(self.id))
-        for key in self.dist:
-            print(key)
-            print(self.dist[key].cost)
-            print(self.dist[key].path)
-
-        if dv_updated == True:
+        if self.recalculate_dist() == True:
             self.broadcast_change(0)
 
     # Return a neighbor, -1 if no path to destination
@@ -113,10 +67,44 @@ class Distance_Vector_Node(Node):
             return self.dist[destination].path[1]
         else:
             return -1
+
+    def recalculate_dist(self):
+        dv_updated = False
+
+        print("old dv at node " + str(self.id))
+        for key in self.dist:
+            print(key)
+            print(self.dist[key].cost)
+            print(self.dist[key].path)
+
+        for key in self.dist: # loop through all current entries in DV
+            curr_best = self.dist[key].cost
+
+            # checking for updated distances
+            for neighbor in self.ndist: # checking all neighbors' DVs
+                if key in self.ndist[neighbor] and key != neighbor: # if this neighbor has a path to the destination
+                    # we also don't want to copy the "identification entry in DV" (cost of n to n is 0)
+                    if curr_best > self.ndist[neighbor][key].cost + self.dist[neighbor].cost: # quicker path to mykey via neighbor
+                        dv_updated = True
+                        self.dist[key] = copy.deepcopy(self.ndist[neighbor][key])
+                        self.dist[key].cost += self.dist[neighbor].cost
+                        self.dist[key].path.insert(0,self.id)
+        
+
+        print("NEW dv at node " + str(self.id))
+        for key in self.dist:
+            print(key)
+            print(self.dist[key].cost)
+            print(self.dist[key].path)
+
+        return dv_updated
+        
     
+    # json.dumps can't handle classes by default, so this helper function (found online) was written to faciliate that
     def to_json(self, obj):
         return json.dumps(obj, default=lambda obj: obj.__dict__)
 
+    # when n=0, broadcast to all neighbors, else n is specific neighbor to broadcast to
     def broadcast_change(self, n):
         dict_json = self.to_json(self.dist)
         m = json.dumps((self.id, dict_json))
