d804794f74524b3cccefd64e1e6d896a98bc67df
zhuk48 <33162506+zhuk48@users.noreply.github.com>
2022-02-17 18:09:35 -0600

    temp push

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -1,12 +1,13 @@
+from operator import ne
 from simulator.node import Node
 import copy
 import json
 
 #Why doesn't Python have structs like C???
 # self.dist:
-# set(a,b) : {DV}
+# b : {DV}
 # DV list:
-# [cost_ab, path_ab , seq_ab]
+# [cost_ab, path_ab , seq]
 
 class Distance_Vector_Node(Node):
     def __init__(self, id):
@@ -19,31 +20,37 @@ class Distance_Vector_Node(Node):
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        temp_set = frozenset(self.id, neighbor)
-        
-        if temp_set in self.dist.keys(): #link already exists, updating value
+        if neighbor in self.dist.keys(): #link already exists, updating value
             # latency = -1 if delete a link
             if latency == -1:
-                del self.dist[temp_set]
+                del self.dist[neighbor]
             else:
-                change = self.dist[temp_set][0] - latency
+                change = self.dist[neighbor][0] - latency
                 # only update if change
                 if change != 0:
                     # recalculate DV's
                     # specfically recalculate DV's for any path that travels through updated node
                     for key in self.dist:
-                        if latency in self.dist[key][1]: # nodes where the updated link is incl in path
+                        if neighbor in self.dist[key][1]: # nodes where the updated link is incl in path
                             self.dist[key][0] = self.dist[key][0] - change
+                            self.dist[key][2] += 1
             self.broadcast_change()
                      
         else: #link DNE, create new one
-            self.dist[temp_set][0] = latency 
-            self.dist[temp_set][1] = [self.id, neighbor]
-            self.dist[temp_set][2] = 0
+            self.dist[neighbor] = []
+            self.dist[neighbor][0] = latency 
+            self.dist[neighbor][1] = [self.id, neighbor]
+            self.dist[neighbor][2] = 0
             self.broadcast_change()
 
     def process_incoming_routing_message(self, m):
-        pass
+        n = m.loads(m)[0]
+        new_table = m.loads(m)[1]
+        if not self.dist == new_table:
+            ## NEED TO DO
+            pass
+
+
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
@@ -55,5 +62,5 @@ class Distance_Vector_Node(Node):
     
 
     def broadcast_change(self):
-        m = json.dumps(self.dist)
+        m = json.dumps(self.id, self.dist)
         self.send_to_neighbors(m)
